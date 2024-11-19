3c0f84484884b6e38defaa91454bddbf2205ffa4
Björn Hägglund <bjorn.hagglund@nav.no>
2024-11-19 10:55:31 +0100

    Proceed in normal modus

--- src/main/kotlin/no/nav/sf/pdl/kafka/Bootstrap.kt
+++ src/main/kotlin/no/nav/sf/pdl/kafka/Bootstrap.kt
@@ -4,7 +4,7 @@ val devContext = env(config_DEPLOY_CLUSTER) == "dev-gcp"
 
 val application = when (env(config_DEPLOY_APP)) {
     "sf-pdl-kafka" -> KafkaPosterApplication(
-        filter = if (devContext) ::isTombstoneOrSalesforceTagged else ::cherryPickOffsets,
+        filter = if (devContext) ::isTombstoneOrSalesforceTagged else null,
         modifier = ::reduceByWhitelistAndRemoveHistoricalItems
     )
     "sf-geografisktilknytning" -> KafkaPosterApplication(
